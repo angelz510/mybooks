e9571ca20adf1bef9ef24be6b28aed4f66ab9e0f
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-08-10 17:25:15 -0700

    axios call to fetch books

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const bookSearch = async () => {
+      const results = await axios(
+        `https://www.googleapis.com/books/v1/volumes?q=react&key=${process.env.REACT_APP_API_KEY}`
+      );
+      console.log(results.data);
+    };
+    bookSearch();
+  }, []);
   return <div className="App"></div>;
 }
 
