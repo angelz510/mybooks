c6e904590c3f6eeca96162132abe5b81c7876602
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-08-16 21:17:18 -0700

    style books card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./Card.css";
 
 const Card = ({ book }) => {
-  console.log("book ==> ", book);
   return (
     <>
       {book.map((item) => {
         let thumbnail =
-          item.volumeInfo.imageLinks &&
-          item.volumeInfo.imageLinks.smallThumbnail;
+          item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail;
         if (thumbnail !== undefined) {
           return (
             <div className="books-card" key={item.id}>
               <img src={thumbnail} alt="" />
               <div className="book-details">
                 <h3 className="book-title">{item.volumeInfo.title}</h3>
+                <p className="book-author">Author: {item.volumeInfo.authors}</p>
+                <p>Publish Date: {item.volumeInfo.publishedDate}</p>
               </div>
             </div>
           );
